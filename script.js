8a1d1379ed7ec6f49ac9b990a10ac7b9dad1f47e
Feranmi Akinyele <akinyeleferanmi19@gmail.com>
2025-02-21 16:07:05 +0100

    first commit

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
 function submit(event){
     event.preventDefault()
 
-    let income= parseInt(document.getElementById('income').value)
+    let income= document.getElementById('income').value
     let income_err=document.getElementById('income_err')
-    let balance= parseInt(document.getElementById('balance').value)
+    let balance=document.getElementById('balance').value
     let balance_err= document.getElementById('balance_err')
-    let loan = parseInt(document.getElementById('loan').value)
+    let loan = document.getElementById('loan').value
     let loan_err = document.getElementById('loan_err')
     let depositDate= document.getElementById('depositDate').value
     let depositDate_err= document.getElementById('depositDate_err')
     let collection= document.getElementById('collection').value
     let collection_err= document.getElementById('collection_err')
-    let repayment= parseInt(document.getElementById('repayment').value)
+    let repayment= document.getElementById('repayment').value
     let repayment_err= document.getElementById('repayment_err')
-    // let acct_type= document.getElementById('acct_type').value
-    // let acct_type_err= document.getElementById('acct_type_err')
     let select = document.getElementById('select')
-    let loan_confirm = 45/100 * income
+    let loan_confirm = 45/100 * parseInt(income)
     let points = 0
     let newDate = new Date(depositDate)
     let newMonth = newDate.getMonth()
@@ -24,12 +22,11 @@ function submit(event){
     let presentDate = new Date()
     let presentMonth = presentDate.getMonth()
     let converted_1 =presentMonth.toString()
-    console.log(typeof(income))
     
     if(income ===''){
         income_err.innerText = '*This Field is Required'
     }else if(loan>loan_confirm){
-        console.log('gh')
+        alert('Loan cannot be more than 45% of interest')
     }else{
         income_err.innerText = ''
     }
@@ -41,11 +38,11 @@ function submit(event){
     }
 
     if(balance === ''){
-        balance_err.innerText = '*This Field is Required'
-    }else if(balance>loan){
+        balance_err.textContent = '*This Field is Required'
+    }else if(parseInt(balance)>parseInt(loan)){
         points += 10
         console.log(points)
-    }else if(balance<loan){
+    }else if(parseInt(balance)<parseInt(loan)){
         points -= 10
         console.log(points)
     }else{
@@ -59,40 +56,49 @@ function submit(event){
     }
 
     if(converted_1 === converted){
-        console.log('confirm')
         points += 5
-        console.log(points)
     }else{
         points += 0
     }
     // fourth input
-
-
-    if(repayment ===''){
-        repayment_err.innerText = '*This Field is Required'
-    }else if(repayment<6){
-        points += 5
+    
+    if(collection === ''){
+        collection_err.innerText = '*This Field is Required'
+    }else if(collection >= 6){
+        points += 10
         console.log(points)
-    }else if(repayment>6){
+    }else if(collection < 6){
         points += 0
+        console.log(points)
+    }else{
+         collection_err.innerText = ''
+    }
+    
+    if(repayment === ''){
+        repayment_err.innerText='*This Field is Required'
+    }else if(repayment < 6){
+        points += 10
+        console.log(points)
+    }else if(repayment >= 6){
+        points += 0
+        console.log(points)
     }else{
         repayment_err.innerText = ''
     }
 
-    if(select.value ===""){
+    
+    
+    if(select.value === ""){
         acct_type_err.innerText = '*This Field is Required'
     }else if(select.value === 'Savings'){
-        console.log('sharp')
         points += 5
         console.log(points)
     }else if(select.value === 'Current'){
-        console.log('blunt')
         points += 10
         console.log(points)
     }else{
         acct_type_err = ''
     }
-
 
     if(points>=30){
         alert('Congratulations, loan has been granted')
@@ -100,5 +106,6 @@ function submit(event){
     }else{
         alert('Sorry, loan not granted')
     }
+
     
 }
